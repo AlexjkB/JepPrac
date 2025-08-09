3429236ea9bbdbd0da934456ba2d9d2125a68393
AlexjkB <alexjkbaker@gmail.com>
2025-08-08 20:19:30 -0500

    remove

--- jeopardy-practice/next.config.ts
+++ jeopardy-practice/next.config.ts
@@ -4,19 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["upload.wikimedia.org"],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)", 
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
